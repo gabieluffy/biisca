51d7685297a184d4db2b5b368490919d5322ee36
gabieluffy <131404044+gabieluffy@users.noreply.github.com>
2023-05-01 13:26:27 -0300

    Add files via upload
    Criação do médoto embaralhar

--- Jogo.java
+++ Jogo.java
@@ -1,19 +1,27 @@
-package bisca;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Jogo {
-  private List<Jogador> jogadores;
-  private Baralho baralho;
-  public Jogo(List<String> nomesJogadores) {
-   jogadores = nomesJogadores.stream().map(Jogador::new).collect(Collectors.toList());
-   baralho = new Baralho();
-		    }
+    private List<Jogador> jogadores;
+    private Baralho baralho;
 
-	}
+    public Jogo(List<String> nomesJogadores) {
+        jogadores = nomesJogadores.stream().map(Jogador::new).collect(Collectors.toList());
+        baralho = new Baralho();
+    }
 
+    public void jogar() {
+        baralho.embaralhar();
 
+        for (Jogador jogador : jogadores) {
+            jogador.receberCarta(baralho.comprarCarta());
+        }
 
+        for (int i = 0; i < 4; i++) {
+            List<Carta> cartasRodada = jogadores.stream()
+                    .map(jogador -> jogador.jogarCarta(i))
+                    .filter(carta -> carta != null)
+                    .collect(Collectors.toList());
